8e896e19890d90e972a6ea31918157704558c095
Somushiva162124 <somushivakumar057@gmail.com>
2026-09-06 21:54:57 +0530

    Add Django admin model registrations

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile, Knowledge, Research, Media, PolarLocation
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Knowledge)
+admin.site.register(Research)
+admin.site.register(Media)
+admin.site.register(PolarLocation)
